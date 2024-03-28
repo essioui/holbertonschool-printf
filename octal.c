af12cdbef24c66ff9e7d4159d0d5882ef22e6ff0
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-28 10:36:27 +0100

    print octal corrections

--- octal.c
+++ octal.c
@@ -4,21 +4,9 @@
 */
 void print_oct(unsigned int n)
 {
-    int i = 0, tmp = 0, j;
-    int arr[100];
-
-    if (n == 0)
-    _putchar('0');
-    while (n != 0)
+    if (n >= 8)
     {
-        tmp = n % 8;
-        if (tmp < 8)
-        arr[i] = 48 + tmp;
-        n = n / 16;
-        i++;  
+        print_oct(n / 8);
     }
-
-    for (j = i - 1; j >= 0; j--)
-    _putchar(arr[j]);
-    
+    _putchar('0' + (n % 8));
 }
