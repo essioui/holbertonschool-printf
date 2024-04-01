4cb1e2d589e2da32af717d8980a3e90f53bc3e17
essioui <salahessioui@gmail.com>
2024-04-01 14:10:52 +0100

    mess

--- print_numbers.c
+++ print_numbers.c
@@ -1,47 +1,29 @@
 #include "main.h"
 /**
- * p_dec - prints decimal
- * @arg: argument to print
- * Return: number of characters printed
- */
-int p_dec(va_list arg)
+* print_number - print numbers
+* @n: integer
+*/
+void print_number(int n)
 {
-	int n = va_arg(arg, int);
-	int num;
-	int last = n % 10;
-	int dig;
-	int exp = 1;
-	int i = 1;
+int i = 10;
+unsigned int p;
+p = n;
+if (n < 0)
+{
+p = -n;
+_putchar('-');
+}
+while (p / i > 9)
+{
+i *= 10;
+}
+if (p > 9)
+while (i >= 10)
+{
+_putchar(p / i + '0');
+p = p % i;
+i /= 10;
+}
+_putchar(p % 10 + '0');
 
-	n = n / 10;
-	num = n;
-
-	if (last < 0)
-	{
-		_putchar('-');
-		num = -num;
-		n = -n;
-		last = -last;
-		i++;
-	}
-	if (num > 0)
-	{
-		while (num / 10 != 0)
-		{
-			exp = exp * 10;
-			num = num / 10;
-		}
-		num = n;
-		while (exp > 0)
-		{
-			dig = num / exp;
-			_putchar(dig + '0');
-			num = num - (dig * exp);
-			exp = exp / 10;
-			i++;
-		}
-	}
-	_putchar(last + '0');
-
-	return (i);
 }
