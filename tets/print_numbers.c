e633efac36f2b5edfb8abc40967d34627f240bab
essioui <salahessioui@gmail.com>
2024-04-01 13:19:55 +0100

    numbers

--- tets/print_numbers.c
+++ tets/print_numbers.c
@@ -1,66 +1,29 @@
 #include "main.h"
-
 /**
- * print_number - prints a number send to this function
- * @args: List of arguments
- * Return: The number of arguments printed
- */
-int print_number(va_list args)
+* print_number - print numbers
+* @n: integer
+*/
+void print_number(int n)
 {
-	int n;
-	int div;
-	int len;
-	unsigned int num;
-
-	n  = va_arg(args, int);
-	div = 1;
-	len = 0;
-
-	if (n < 0)
-	{
-		len += _write_char('-');
-		num = n * -1;
-	}
-	else
-		num = n;
-
-	for (; num / div > 9; )
-		div *= 10;
-
-	for (; div != 0; )
-	{
-		len += _write_char('0' + num / div);
-		num %= div;
-		div /= 10;
-	}
-
-	return (len);
+int i = 10;
+unsigned int p;
+p = n;
+if (n < 0)
+{
+p = -n;
+_putchar('-');
 }
-/**
- * print_unsgined_number - Prints an unsigned number
- * @n: unsigned integer to be printed
- * Return: The amount of numbers printed
- */
-int print_unsgined_number(unsigned int n)
+while (p / i > 9)
 {
-	int div;
-	int len;
-	unsigned int num;
+i *= 10;
+}
+if (p > 9)
+while (i >= 10)
+{
+_putchar(p / i + '0');
+p = p % i;
+i /= 10;
+}
+_putchar(p % 10 + '0');
 
-	div = 1;
-	len = 0;
-
-	num = n;
-
-	for (; num / div > 9; )
-		div *= 10;
-
-	for (; div != 0; )
-	{
-		len += _write_char('0' + num / div);
-		num %= div;
-		div /= 10;
-	}
-
-	return (len);
 }
